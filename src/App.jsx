511b03a09fca8ba6acd2ca00ddbb491506264530
Phoenix7290 <b5261064@gmail.com>
2024-08-06 16:46:19 -0300

    Primeiro Commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+
+export default function App() {
+    return (
+        <main>
+            <a href="src/TP1/02/index.html">TP 1.1</a>
+            <a href="src/TP1/02/index.html">TP 1.2</a>
+            <a href="src/TP1/03/index.html">TP 1.3</a>
+            <a href="src/TP1/04/index.html">TP 1.4</a>
+            <a href="src/TP1/05/index.html">TP 1.5</a>
+            <a href="src/TP1/06/index.html">TP 1.6</a>
+            <a href="src/TP1/07/index.html">TP 1.7</a>
+            <a href="src/TP1/08/index.html">TP 1.8</a>
+            <a href="src/TP1/09/index.html">TP 1.9</a>
+            <a href="src/TP1/10/index.html">TP 1.10</a>
+            <a href="src/TP1/11/index.html">TP 1.11</a>
+            <a href="src/TP1/12/index.html">TP 1.12</a>
+            <a href="src/TP1/13/index.html">TP 1.13</a>
+            <a href="src/TP1/14/index.html">TP 1.14</a>
+            <a href="src/TP1/15/index.html">TP 1.15</a>
+            <a href="src/TP1/16/index.html">TP 1.16</a>
+        </main>
+    )
+}
---
+++ src/TP1/10/App.jsx
@@ -0,0 +1,5 @@
+export default function App() {
+    return (
+        <div>Faça você mesmo!</div>
+    )
+}
