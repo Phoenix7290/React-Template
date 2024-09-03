57493c16a726b2133951667ff36116a3baa0d17c
Phoenix7290 <b5261064@gmail.com>
2024-09-03 19:32:09 -0300

    TP3

--- src/App.jsx
+++ src/App.jsx
@@ -45,6 +45,27 @@ export default function App() {
                     <a href="src/TP2/16/index.html">TP 2.16</a>
                 </div>
             </section>
+            <section>
+                <h1>TP 3</h1>
+                <div className='container'>
+                    <a href="src/TP3/01/index.html">TP 3.1</a>
+                    <a href="src/TP3/02/index.html">TP 3.2</a>
+                    <a href="src/TP3/03/index.html">TP 3.3</a>
+                    <a href="src/TP3/04/index.html">TP 3.4</a>
+                    <a href="src/TP3/05/index.html">TP 3.5</a>
+                    <a href="src/TP3/06/index.html">TP 3.6</a>
+                    <a href="src/TP3/07/index.html">TP 3.7</a>
+                    <a href="src/TP3/08/index.html">TP 3.8</a>
+                    <a href="src/TP3/09/index.html">TP 3.9</a>
+                    <a href="src/TP3/10/index.html">TP 3.10</a>
+                    <a href="src/TP3/11/index.html">TP 3.11</a>
+                    <a href="src/TP3/12/index.html">TP 3.12</a>
+                    <a href="src/TP3/13/index.html">TP 3.13</a>
+                    <a href="src/TP3/14/index.html">TP 3.14</a>
+                    <a href="src/TP3/15/index.html">TP 3.15</a>
+                    <a href="src/TP3/16/index.html">TP 3.16</a>
+                </div>
+            </section>
         </main>
     )
 }
