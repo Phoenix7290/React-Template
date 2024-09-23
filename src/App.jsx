6068db5eae775883ff9901e8ad494151de2156a0
Phoenix7290 <b5261064@gmail.com>
2024-09-23 11:35:05 -0300

    build fixes

--- src/App.jsx
+++ src/App.jsx
@@ -6,64 +6,64 @@ export default function App() {
             <section>
                 <h1>TP 1</h1>
                 <div className='container'>
-                <a href="src/TP1/01/index.html">TP 1.1</a>
-                    <a href="src/TP1/02/index.html">TP 1.2</a>
-                    <a href="src/TP1/03/index.html">TP 1.3</a>
-                    <a href="src/TP1/04/index.html">TP 1.4</a>
-                    <a href="src/TP1/05/index.html">TP 1.5</a>
-                    <a href="src/TP1/06/index.html">TP 1.6</a>
-                    <a href="src/TP1/07/index.html">TP 1.7</a>
-                    <a href="src/TP1/08/index.html">TP 1.8</a>
-                    <a href="src/TP1/09/index.html">TP 1.9</a>
-                    <a href="src/TP1/10/index.html">TP 1.10</a>
-                    <a href="src/TP1/11/index.html">TP 1.11</a>
-                    <a href="src/TP1/12/index.html">TP 1.12</a>
-                    <a href="src/TP1/13/index.html">TP 1.13</a>
-                    <a href="src/TP1/14/index.html">TP 1.14</a>
-                    <a href="src/TP1/15/index.html">TP 1.15</a>
-                    <a href="src/TP1/16/index.html">TP 1.16</a>
+                    <a href={`${import.meta.env.BASE_URL}src/TP1/01/index.html`}>TP 1.1</a>
+                    <a href={`${import.meta.env.BASE_URL}src/TP1/02/index.html`}>TP 1.2</a>
+                    <a href={`${import.meta.env.BASE_URL}src/TP1/03/index.html`}>TP 1.3</a>
+                    <a href={`${import.meta.env.BASE_URL}src/TP1/04/index.html`}>TP 1.4</a>
+                    <a href={`${import.meta.env.BASE_URL}src/TP1/05/index.html`}>TP 1.5</a>
+                    <a href={`${import.meta.env.BASE_URL}src/TP1/06/index.html`}>TP 1.6</a>
+                    <a href={`${import.meta.env.BASE_URL}src/TP1/07/index.html`}>TP 1.7</a>
+                    <a href={`${import.meta.env.BASE_URL}src/TP1/08/index.html`}>TP 1.8</a>
+                    <a href={`${import.meta.env.BASE_URL}src/TP1/09/index.html`}>TP 1.9</a>
+                    <a href={`${import.meta.env.BASE_URL}src/TP1/10/index.html`}>TP 1.10</a>
+                    <a href={`${import.meta.env.BASE_URL}src/TP1/11/index.html`}>TP 1.11</a>
+                    <a href={`${import.meta.env.BASE_URL}src/TP1/12/index.html`}>TP 1.12</a>
+                    <a href={`${import.meta.env.BASE_URL}src/TP1/13/index.html`}>TP 1.13</a>
+                    <a href={`${import.meta.env.BASE_URL}src/TP1/14/index.html`}>TP 1.14</a>
+                    <a href={`${import.meta.env.BASE_URL}src/TP1/15/index.html`}>TP 1.15</a>
+                    <a href={`${import.meta.env.BASE_URL}src/TP1/16/index.html`}>TP 1.16</a>
                 </div>
             </section>
             <section>
                 <h1>TP 2</h1>
                 <div className='container'>
-                    <a href="src/TP2/01/index.html">TP 2.1</a>
-                    <a href="src/TP2/02/index.html">TP 2.2</a>
-                    <a href="src/TP2/03/index.html">TP 2.3</a>
-                    <a href="src/TP2/04/index.html">TP 2.4</a>
-                    <a href="src/TP2/05/index.html">TP 2.5</a>
-                    <a href="src/TP2/06/index.html">TP 2.6</a>
-                    <a href="src/TP2/07/index.html">TP 2.7</a>
-                    <a href="src/TP2/08/index.html">TP 2.8</a>
-                    <a href="src/TP2/09/index.html">TP 2.9</a>
-                    <a href="src/TP2/10/index.html">TP 2.10</a>
-                    <a href="src/TP2/11/index.html">TP 2.11</a>
-                    <a href="src/TP2/12/index.html">TP 2.12</a>
-                    <a href="src/TP2/13/index.html">TP 2.13</a>
-                    <a href="src/TP2/14/index.html">TP 2.14</a>
-                    <a href="src/TP2/15/index.html">TP 2.15</a>
-                    <a href="src/TP2/16/index.html">TP 2.16</a>
+                    <a href={`${import.meta.env.BASE_URL}src/TP2/01/index.html`}>TP 2.1</a>
+                    <a href={`${import.meta.env.BASE_URL}src/TP2/02/index.html`}>TP 2.2</a>
+                    <a href={`${import.meta.env.BASE_URL}src/TP2/03/index.html`}>TP 2.3</a>
+                    <a href={`${import.meta.env.BASE_URL}src/TP2/04/index.html`}>TP 2.4</a>
+                    <a href={`${import.meta.env.BASE_URL}src/TP2/05/index.html`}>TP 2.5</a>
+                    <a href={`${import.meta.env.BASE_URL}src/TP2/06/index.html`}>TP 2.6</a>
+                    <a href={`${import.meta.env.BASE_URL}src/TP2/07/index.html`}>TP 2.7</a>
+                    <a href={`${import.meta.env.BASE_URL}src/TP2/08/index.html`}>TP 2.8</a>
+                    <a href={`${import.meta.env.BASE_URL}src/TP2/09/index.html`}>TP 2.9</a>
+                    <a href={`${import.meta.env.BASE_URL}src/TP2/10/index.html`}>TP 2.10</a>
+                    <a href={`${import.meta.env.BASE_URL}src/TP2/11/index.html`}>TP 2.11</a>
+                    <a href={`${import.meta.env.BASE_URL}src/TP2/12/index.html`}>TP 2.12</a>
+                    <a href={`${import.meta.env.BASE_URL}src/TP2/13/index.html`}>TP 2.13</a>
+                    <a href={`${import.meta.env.BASE_URL}src/TP2/14/index.html`}>TP 2.14</a>
+                    <a href={`${import.meta.env.BASE_URL}src/TP2/15/index.html`}>TP 2.15</a>
+                    <a href={`${import.meta.env.BASE_URL}src/TP2/16/index.html`}>TP 2.16</a>
                 </div>
             </section>
             <section>
                 <h1>TP 3</h1>
                 <div className='container'>
-                    <a href="src/TP3/01/index.html">TP 3.1</a>
-                    <a href="src/TP3/02/index.html">TP 3.2</a>
-                    <a href="src/TP3/03/index.html">TP 3.3</a>
-                    <a href="src/TP3/04/index.html">TP 3.4</a>
-                    <a href="src/TP3/05/index.html">TP 3.5</a>
-                    <a href="src/TP3/06/index.html">TP 3.6</a>
-                    <a href="src/TP3/07/index.html">TP 3.7</a>
-                    <a href="src/TP3/08/index.html">TP 3.8</a>
-                    <a href="src/TP3/09/index.html">TP 3.9</a>
-                    <a href="src/TP3/10/index.html">TP 3.10</a>
-                    <a href="src/TP3/11/index.html">TP 3.11</a>
-                    <a href="src/TP3/12/index.html">TP 3.12</a>
-                    <a href="src/TP3/13/index.html">TP 3.13</a>
-                    <a href="src/TP3/14/index.html">TP 3.14</a>
-                    <a href="src/TP3/15/index.html">TP 3.15</a>
-                    <a href="src/TP3/16/index.html">TP 3.16</a>
+                    <a href={`${import.meta.env.BASE_URL}src/TP3/01/index.html`}>TP 3.1</a>
+                    <a href={`${import.meta.env.BASE_URL}src/TP3/02/index.html`}>TP 3.2</a>
+                    <a href={`${import.meta.env.BASE_URL}src/TP3/03/index.html`}>TP 3.3</a>
+                    <a href={`${import.meta.env.BASE_URL}src/TP3/04/index.html`}>TP 3.4</a>
+                    <a href={`${import.meta.env.BASE_URL}src/TP3/05/index.html`}>TP 3.5</a>
+                    <a href={`${import.meta.env.BASE_URL}src/TP3/06/index.html`}>TP 3.6</a>
+                    <a href={`${import.meta.env.BASE_URL}src/TP3/07/index.html`}>TP 3.7</a>
+                    <a href={`${import.meta.env.BASE_URL}src/TP3/08/index.html`}>TP 3.8</a>
+                    <a href={`${import.meta.env.BASE_URL}src/TP3/09/index.html`}>TP 3.9</a>
+                    <a href={`${import.meta.env.BASE_URL}src/TP3/10/index.html`}>TP 3.10</a>
+                    <a href={`${import.meta.env.BASE_URL}src/TP3/11/index.html`}>TP 3.11</a>
+                    <a href={`${import.meta.env.BASE_URL}src/TP3/12/index.html`}>TP 3.12</a>
+                    <a href={`${import.meta.env.BASE_URL}src/TP3/13/index.html`}>TP 3.13</a>
+                    <a href={`${import.meta.env.BASE_URL}src/TP3/14/index.html`}>TP 3.14</a>
+                    <a href={`${import.meta.env.BASE_URL}src/TP3/15/index.html`}>TP 3.15</a>
+                    <a href={`${import.meta.env.BASE_URL}src/TP3/16/index.html`}>TP 3.16</a>
                 </div>
             </section>
         </main>
